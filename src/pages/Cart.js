eb261d1537964947f73ab53a0644a83964fa249c
kunal_ingawale <81668653+kunal232i@users.noreply.github.com>
2022-12-30 18:57:21 +0000

    initial setup

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Button, ButtonGroup, Card, HStack, Image, VStack, Text, Flex} from '@chakra-ui/react'
+import img1 from "../components/Images/nodeMCU.jpeg"
+import img2 from "../components/Images/arduinoUNO.jpeg"
+
+const Imgs = { img1, img2}
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <>
+    <Flex p={10} justify="space-between">
+      <Button  bgColor={"grey"}>CONTINUE SHOPPING</Button>
+      <Card p={"5"}>
+        <Text fontWeight={"bold"} color={"black"}> Product Summary</Text>
+        <Button bgColor={"grey"}>CHECK OUT</Button>
+      </Card>
+      </Flex>
+    
+    <VStack p={5}>
+      <Card p={5}>
+        <Image src={Imgs.img1}/>
+      </Card>
+      <Card p={5}>
+        <Image src={Imgs.img2}/>
+      </Card>
+      <Card p={5}>
+        <Image src={Imgs.img1}/>
+      </Card>
+    </VStack>
+    
+    </>
+  );
 };
 
 export default Cart;
